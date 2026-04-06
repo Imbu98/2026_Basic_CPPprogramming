5652a4ffe5431e204b598b531037d1ebbad7ed32
DESKTOP-QT9VDHF\Admin <gyxor98@gmail.com>
2026-04-06 17:04:31 +0900

    두번째커밋

---
+++ L01_standard_IO/L01_standard_IO.cpp
@@ -0,0 +1,16 @@
+// 전처리기
+#include <cstdio>
+
+// 모든 C/C++로 구성된 프로그램은 한 개 이상의 함수로 구성 -> 프로그램 실행시 진입점(entry Point)를 알아야 하기 때문에
+// 이중 반드시 있어야 하는 함수, main()함수
+// 없으면 컴파일 자체가 안됨.
+
+int main()
+{
+
+    printf("Hello World\n");
+    
+    printf("Hello World");
+     
+    return 0;
+}
